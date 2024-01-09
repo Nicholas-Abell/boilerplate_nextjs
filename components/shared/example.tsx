df9273e2ef7cbb4053424afb61c573bba11100e3
Nick <darkforceyoda@gmail.com>
2024-01-09 10:43:01 -0600

    folder structure

---
+++ components/shared/example.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type exampleProps = {
+    
+};
+
+const example:React.FC<exampleProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default example;
